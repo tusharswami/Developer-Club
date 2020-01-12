5f55d71bada849c1efeb764e9fb16c675c5f7cdb
tusharswami <tusharswami01@gmail.com>
2020-01-12 15:41:25 +0530

    Intial Commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => res.send("API Running"))
+
+const PORT = process.env.port || 5000;
+app.listen(PORT, ()=> console.log(`Server Started at ${PORT}`));
