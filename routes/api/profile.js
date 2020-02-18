d73f08ed77340b6c08cc5048caf34d2621a2a8de
tusharswami <tusharswami01@gmail.com>
2020-02-18 23:26:29 +0530

    Profile Routes Completed

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const config = require("config");
+const request = require('request');
 const auth = require("../../middleware/auth");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -286,4 +288,31 @@ router.delete('/education/:edu_id', auth, async (req, res)=>{
         res.status(500).send("Server Error");
     }
 });
+
+// @route   GET api/profile/github/:username
+// @desc    List GitHub Repositories for the username
+// @access  Public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubID')}&client_secret=${config.get('githubSecret')}`,
+            method : 'GET',
+            headers : { "user-agent" : "node.js"}
+        }
+
+        request(options, (error, response, body) => {
+            if(error) return console.log(err.message);
+            if(response.statusCode !== 200){
+                return res.status(404).send("Repo Not Found");
+            }
+            res.json(JSON.parse(body));
+        })
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+})
 module.exports = router;
